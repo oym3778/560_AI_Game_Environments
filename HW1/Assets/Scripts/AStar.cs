3f4b0f1b0a17d8fa03472829b5754ed0013695a4
oym3778 <oym3778@g.rit.edu>
2025-02-12 23:54:01 -0500

    initial commit

---
+++ HW1/Assets/Scripts/AStar.cs
@@ -0,0 +1,57 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Performs search using A*.
+/// </summary>
+public class AStar : MonoBehaviour
+{
+    // Colors for the different search categories.
+    public static Color openColor = Color.cyan;
+    public static Color closedColor = Color.blue;
+    public static Color activeColor = Color.yellow;
+    public static Color pathColor = Color.yellow;
+
+    // The stopwatch for timing search.
+    private static Stopwatch watch = new Stopwatch();
+
+    public static IEnumerator search(GameObject start, GameObject end, Heuristic heuristic, float waitTime, bool colorTiles = false, bool displayCosts = false, Stack<NodeRecord> path = null)
+    {
+        // Starts the stopwatch.
+        watch.Start();
+
+        // Add your A* code here.
+
+        // Stops the stopwatch.
+        watch.Stop();
+
+        UnityEngine.Debug.Log("Seconds Elapsed: " + (watch.ElapsedMilliseconds / 1000f).ToString());
+        UnityEngine.Debug.Log("Nodes Expanded: " + "print the number of nodes expanded here.");
+
+        // Reset the stopwatch.
+        watch.Reset();
+
+        // Determine whether A* found a path and print it here.
+
+        yield return null;
+    }
+
+    public delegate float Heuristic(GameObject start, GameObject tile, GameObject goal);
+
+    public static float Uniform (GameObject start, GameObject tile, GameObject goal)
+    {
+        return 0f;
+    }
+
+    public static float Manhattan (GameObject start, GameObject tile, GameObject goal)
+    {
+        return 0f;
+    }
+
+    public static float CrossProduct (GameObject start, GameObject tile, GameObject goal)
+    {
+        return 0f;
+    }
+}
---
+++ HW1/Assets/Scripts/Dijkstra.cs
@@ -0,0 +1,65 @@
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Performs search using Dijkstra's algorithm.
+/// </summary>
+public class Dijkstra : MonoBehaviour
+{
+    // Colors for the different search categories.
+    public static Color openColor = Color.cyan;
+    public static Color closedColor = Color.blue;
+    public static Color activeColor = Color.yellow;
+    public static Color pathColor = Color.yellow;
+
+    // The stopwatch for timing search.
+    private static Stopwatch watch = new Stopwatch();
+
+    public static IEnumerator search(GameObject start, GameObject end, float waitTime, bool colorTiles = false, bool displayCosts = false, Stack<NodeRecord> path = null)
+    {
+        // Starts the stopwatch.
+        watch.Start();
+        
+        // Add your Dijkstra code here.
+
+        // Stops the stopwatch.
+        watch.Stop();
+
+        UnityEngine.Debug.Log("Seconds Elapsed: " + (watch.ElapsedMilliseconds / 1000f).ToString());
+        UnityEngine.Debug.Log("Nodes Expanded: " + "print the number of nodes expanded here.");
+
+        // Reset the stopwatch.
+        watch.Reset();
+
+        // Determine whether Dijkstra found a path and print it here.
+
+        yield return null;
+    }
+}
+
+/// <summary>
+/// A class for recording search statistics.
+/// </summary>
+public class NodeRecord
+{
+    // The tile game object.
+    public GameObject Tile { get; set; } = null;
+
+    // Set the other class properties here.
+
+    // Sets the tile's color.
+    public void ColorTile (Color newColor)
+    {
+        SpriteRenderer renderer = Tile.GetComponentInChildren<SpriteRenderer>();
+        renderer.material.color = newColor;
+    }
+
+    // Displays a string on the tile.
+    public void Display (float value)
+    {
+        TextMesh text = Tile.GetComponent<TextMesh>();
+        text.text = value.ToString();
+    }
+}
