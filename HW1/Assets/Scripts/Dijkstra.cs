9eeeb8e73db28091e54322ebe3fefc02adfaf82a
oym3778 <oym3778@g.rit.edu>
2025-03-04 12:01:46 -0500

    work done at home

--- HW1/Assets/Scripts/Dijkstra.cs
+++ HW1/Assets/Scripts/Dijkstra.cs
@@ -47,50 +47,131 @@ public class Dijkstra : MonoBehaviour
 
         // --Initialize the record for the start node.--
         NodeRecord startRecord = new NodeRecord();
+        startRecord.Tile= start;
         startRecord.node = start.GetComponent<Node>();
         // Since we can access the current nodes connections through the
         // Node class we just use a Connections Method in Node records
-            // startRecord.connection = null
+        // startRecord.connection = null
         startRecord.costSoFar = 0;
 
         // --Initialize the open and closed lists.--
         List<NodeRecord> open = new List<NodeRecord>();
         open.Add(startRecord);
         List<NodeRecord> closed = new List<NodeRecord>();
-        
+
         // For coloring
-        SpriteRenderer renderer = null;
+        SpriteRenderer renderer = start.GetComponentInChildren<SpriteRenderer>();
 
-
+        NodeRecord current = new NodeRecord();
+        Dictionary<Direction, GameObject> connections = new Dictionary<Direction, GameObject>();
         // --Iterate through processing each node.--
-         while (open.Count > 0)
+        while (open.Count > 0)
         {
 
             // --Find the smallest element in the open list.--
-            NodeRecord current = SmallestElement(open);
-
+            current = SmallestElement(open);
+            print("This is the smallest node" + current.node.name);
             // --If coloring tiles, update the tile color.--
             if (colorTiles)
             {
                 // Grab the renderer of the clicked tile.
+                //UnityEngine.Debug.Log("Search Failed render");
+                //from = hit.collider.gameObject;
                 renderer = current.node.gameObject.GetComponentInChildren<SpriteRenderer>();
                 // Turn the tile color to magenta to visualize the selection.
                 renderer.material.color = activeColor;
-            }               
+            }
 
             // --Pause the animation to show the new active tile.--
             yield return new WaitForSeconds(waitTime);
 
             // --If it is the goal node, then terminate.--
-            if (current.node == end)
+            if (current.node == end.GetComponent<Node>())
             {
                 break;
             }
 
             // --Otherwise get its outgoing connections.--
-            Dictionary<Direction, GameObject> connections = current.node.Connections;
+            
+            connections = current.node.Connections;
+            print("These are the connections" + connections);
             foreach (GameObject connection in connections.Values)
             {
+                // --Get the cost estimate for the end node.--
+                Node endNode = connection.GetComponent<Node>();
+                float endNodeCost = current.costSoFar + 1f; // connection.getCost() = 1  TODO figure out how to use a cost per Node
+                NodeRecord endNodeRecord = null;
+
+                // -- Skip if the node is closed
+                if (Contains(closed, endNode))
+                {
+                    continue;
+                }
+                // --.. or if it is open and we’ve found a worse route.--
+                else if (Contains(open, endNode))
+                {
+                    // -- Here we find the record in the open list --
+                    // -- corresponding to the endNode. --
+                    endNodeRecord = Find(open, endNode);
+                    if (endNodeRecord.cost <= endNodeCost) // TODO This may cause an error... endNodeRecord.costSoFar != endNodeRecord.cost in the book
+                    {
+                        continue;
+                    }
+                }
+                // --Otherwise we know we’ve got an unvisited node,--
+                // --so make a record for it.--
+                else
+                {
+                    endNodeRecord = new NodeRecord();
+                    endNodeRecord.node = endNode;
+                }
+
+                // -- We’re here if we need to update the node. Update the --
+                // -- cost and connection. --
+                endNodeRecord.cost = endNodeCost;
+                // endNodeRecord.connection = connection; TODO this may cause some error...
+
+                // --If displaying costs, update the tile display.--
+                if (displayCosts)
+                {
+                    // Grab the text mesh off the tile.
+                    TextMesh text = connection.GetComponent<TextMesh>();
+
+                    // Reset its text to be blank.
+                    text.text = "" + endNodeCost;
+                }
+
+                // --And add it to the open list.--
+                if (!Contains(open, endNode))
+                {
+                    open.Add(endNodeRecord);
+                }
+
+                // --If coloring tiles, update the open tile color.--
+                if (colorTiles)
+                {
+                    // Grab the renderer of the clicked tile.
+                    renderer = endNodeRecord.node.gameObject.GetComponentInChildren<SpriteRenderer>();
+                    // Turn the tile color to magenta to visualize the selection.
+                    renderer.material.color = openColor;
+                }
+                // --Pause the animation to show the new open tile.--
+                yield return new WaitForSeconds(waitTime);
+
+                // -- We’ve finished looking at the connections for the current --
+                // -- node, so add it to the closed list and remove it from the --
+                // -- open list. --
+                open.Remove(current);
+                closed.Add(current);
+
+                // --If coloring tiles, update the closed tile color.--
+                if (colorTiles)
+                {
+                    // Grab the renderer of the clicked tile.
+                    renderer = current.node.gameObject.GetComponentInChildren<SpriteRenderer>();
+                    // Turn the tile color to magenta to visualize the selection.
+                    renderer.material.color = closedColor;
+                }
 
             }
         }
@@ -107,6 +188,24 @@ public class Dijkstra : MonoBehaviour
         watch.Reset();
 
         // Determine whether Dijkstra found a path and print it here.
+        // --We’re here if we’ve either found the goal, or if we’ve no more --
+        // --nodes to search, find which. --
+        if (current.node != end)
+        {
+            // --We’ve run out of nodes without finding the goal,--
+            // --so there’s no solution.--
+            UnityEngine.Debug.Log("Search Failed");
+        }
+        else
+        {
+            // --Work back along the path, accumulating connections.--
+            //while (current.node != start)
+            //{
+
+            //}
+            //UnityEngine.Debug.Log("Path Length: " + path.Count.ToString());
+            UnityEngine.Debug.Log("Path Length: 0");
+        }
 
         yield return null;
     }
@@ -117,7 +216,7 @@ public class Dijkstra : MonoBehaviour
     /// </summary>
     public static NodeRecord SmallestElement(List<NodeRecord> records)
     {
-        float lowestCost = records[0].costSoFar;
+       float lowestCost = records[0].costSoFar;
         NodeRecord lowestNodeRecord = new NodeRecord();
 
         foreach (NodeRecord nodeRecord in records)
@@ -130,7 +229,7 @@ public class Dijkstra : MonoBehaviour
             // code block to be executed
         }
         // look through the open list and return the NodeRecord with the lowest cost
-        return lowestNodeRecord;
+        return records[0];
     }
 
     /// <summary>
@@ -138,9 +237,19 @@ public class Dijkstra : MonoBehaviour
     /// NodeRecord structure whose node member is equal to the given parameter.
     /// <param name="node"> A Node Script
     /// </summary>
-    public static void Contains(List<NodeRecord> records, Node node)
+    public static bool Contains(List<NodeRecord> records, Node node)
     {
-        // if the given node is within the open list return the node from the open list
+        // if the given node is within the open list return true
+        foreach (NodeRecord nodeRecord in records)
+        {
+            if (nodeRecord.node == node)
+            {
+                return true;
+            }
+        }
+
+        // else the node wasnt in the list, return false
+        return false;
     }
 
     /// <summary>
@@ -149,7 +258,16 @@ public class Dijkstra : MonoBehaviour
     /// </summary>
     public static NodeRecord Find(List<NodeRecord> records, Node node)
     {
-        return new NodeRecord();
+        foreach (NodeRecord nodeRecord in records)
+        {
+            if (nodeRecord.node == node)
+            {
+                return nodeRecord;
+            }
+        }
+
+        // there was no node within the records...
+        return null;
     }
 
 
@@ -166,11 +284,16 @@ public class NodeRecord
     public Node node { get; set; } = null;
     public float costSoFar { get; set; } = 0;
     public float cost { get; set; } = 1;
+    public Dictionary<Direction, NodeRecord> connections = new Dictionary<Direction, NodeRecord>();
 
-/*    public Dictionary<Direction, GameObject> Connections()
-    {
-        return node.Connections;//new Dictionary<Direction, GameObject>();
-    }*/
+    public Dictionary<Direction, GameObject> getConnections() { 
+        return node.Connections;
+    }
+
+    /*    public Dictionary<Direction, GameObject> Connections()
+        {
+            return node.Connections;//new Dictionary<Direction, GameObject>();
+        }*/
 
     /*// Grab the node scripts attached to the two tile game objects.
     Node fromNode = from.GetComponent<Node>();
