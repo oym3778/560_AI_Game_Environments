d10edab93129dd243812aae85a2f4acf62e2eded
oym3778 <oym3778@g.rit.edu>
2025-03-05 20:49:45 -0500

    initial commit

--- HW1/Assets/Scripts/AStar.cs
+++ HW1/Assets/Scripts/AStar.cs
@@ -1,7 +1,10 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine.Profiling;
 
 /// <summary>
 /// Performs search using A*.
@@ -23,6 +26,146 @@ public class AStar : MonoBehaviour
         watch.Start();
 
         // Add your A* code here.
+        //Heuristic heuristi1c = heuristic.Invoke(start, tile, end);
+
+        int nodesExpanded = 0;
+
+        // --Initialize the record for the start node.--
+        NodeRecord startRecord = new NodeRecord();
+        startRecord.Tile = start;
+        startRecord.node = start.GetComponent<Node>();
+        // Since we can access the current nodes connections through the
+        // Node class we just use a Connections Method in Node records
+        // startRecord.connection = null
+        startRecord.costSoFar = 0;
+
+        // --Initialize the open and closed lists.--
+        List<NodeRecord> open = new List<NodeRecord>();
+        open.Add(startRecord);
+        List<NodeRecord> closed = new List<NodeRecord>();
+        nodesExpanded++;
+
+        // For coloring
+        SpriteRenderer renderer = start.GetComponentInChildren<SpriteRenderer>();
+
+        NodeRecord current = new NodeRecord();
+        Dictionary<Direction, GameObject> connections = new Dictionary<Direction, GameObject>();
+        // --Iterate through processing each node.--
+        while (open.Count > 0)
+        {
+            //nodesExpanded++;
+            // --Find the smallest element in the open list.--
+            current = SmallestElement(open);
+            // --If coloring tiles, update the tile color.--
+            if (colorTiles)
+            {
+                // Grab the renderer of the clicked tile.
+                //UnityEngine.Debug.Log("Search Failed render");
+                //from = hit.collider.gameObject;
+                renderer = current.node.gameObject.GetComponentInChildren<SpriteRenderer>();
+                renderer.material.color = activeColor;
+            }
+
+            // --Pause the animation to show the new active tile.--
+            yield return new WaitForSeconds(waitTime);
+
+            // --If it is the goal node, then terminate.--
+            if (current.node == end.GetComponent<Node>())
+            {
+                break;
+            }
+
+            // --Otherwise get its outgoing connections.--
+            connections = current.node.Connections;
+
+            foreach (GameObject connection in connections.Values)
+            {
+
+                NodeRecord endNodeRecord = null;
+                // --Get the cost estimate for the end node.--
+                Node endNode = connection.GetComponent<Node>(); // this is the to node
+                // add a from node
+                float endNodeCost = current.costSoFar + 1f;
+
+
+                // -- Skip if the node is closed
+                if (Contains(closed, endNode))
+                {
+                    continue;
+                }
+                // --.. or if it is open and we�ve found a worse route.--
+                else if (Contains(open, endNode))
+                {
+                    // -- Here we find the record in the open list --
+                    // -- corresponding to the endNode. --
+                    endNodeRecord = Find(open, endNode);
+                    if (1f <= endNodeCost)
+                    {
+                        continue;
+                    }
+                }
+                // --Otherwise we know we�ve got an unvisited node,--
+                // --so make a record for it.--
+                else
+                {
+                    endNodeRecord = new NodeRecord();
+                    endNodeRecord.node = endNode;
+                }
+
+                // -- We�re here if we need to update the node. Update the --
+                // -- cost and connection. --
+                nodesExpanded++;
+                endNodeRecord.costSoFar = endNodeCost;
+                endNodeRecord.fromNode = current; //TODO this may cause some error...
+
+                // --If displaying costs, update the tile display.--
+                if (displayCosts)
+                {
+                    // Grab the text mesh off the tile.
+                    TextMesh text = connection.GetComponent<TextMesh>();
+
+                    // Reset its text to be blank.
+                    text.text = "" + endNodeCost;
+                }
+
+                // --And add it to the open list.--
+                if (!Contains(open, endNode))
+                {
+                    open.Add(endNodeRecord);
+                }
+
+                // --If coloring tiles, update the open tile color.--
+                if (colorTiles)
+                {
+                    // Grab the renderer of the clicked tile.
+                    renderer = endNodeRecord.node.gameObject.GetComponentInChildren<SpriteRenderer>();
+                    // Turn the tile color to magenta to visualize the selection.
+                    renderer.material.color = openColor;
+                }
+                // --Pause the animation to show the new open tile.--
+                yield return new WaitForSeconds(waitTime);
+
+            }
+
+            // -- We�ve finished looking at the connections for the current --
+            // -- node, so add it to the closed list and remove it from the --
+            // -- open list. --
+            open.Remove(current);
+            closed.Add(current);
+
+            // --If coloring tiles, update the closed tile color.--
+            if (colorTiles)
+            {
+                // Grab the renderer of the clicked tile.
+                renderer = current.node.gameObject.GetComponentInChildren<SpriteRenderer>();
+                // Turn the tile color to magenta to visualize the selection.
+                renderer.material.color = closedColor;
+            }
+        }
+
+
+
+
 
         // Stops the stopwatch.
         watch.Stop();
@@ -35,13 +178,122 @@ public class AStar : MonoBehaviour
 
         // Determine whether A* found a path and print it here.
 
+        // Determine whether A* found a path and print it here.
+        // --We�re here if we�ve either found the goal, or if we�ve no more --
+        // --nodes to search, find which. --
+        if (current.node != end.GetComponent<Node>())
+        {
+            // --We've run out of nodes without finding the goal,--
+            // --so there�s no solution.--
+            UnityEngine.Debug.Log("Search Failed");
+        }
+        else
+        {
+            path = new Stack<NodeRecord>();
+            // --Work back along the path, accumulating connections.--
+            while (current.node != start.GetComponent<Node>())
+            {
+                if (current.node == null) { UnityEngine.Debug.Log("Current node looping back got null, something wrong with fromNode"); }
+                path.Push(current.fromNode);
+                current = current.fromNode;
+
+
+                // --If coloring tiles, update the open tile color.--
+                if (colorTiles)
+                {
+                    // Grab the renderer of the clicked tile.
+                    renderer = current.node.gameObject.GetComponentInChildren<SpriteRenderer>();
+                    // Turn the tile color to magenta to visualize the selection.
+                    renderer.material.color = pathColor;
+                }
+
+                // -- Pause the animation to show the new path tile.
+                // --This is the actual C# command to use.
+                yield return new WaitForSeconds(waitTime);
+            }
+            //UnityEngine.Debug.Log("Path Length: " + path.Count.ToString());
+            UnityEngine.Debug.Log("Path Length: " + path.Count.ToString());
+        }
+
         yield return null;
+    }
+
+    /// <summary>
+    /// The find(node) method returns the NodeRecord structure from the list 
+    /// whose node member is equal to the given parameter.
+    /// </summary>
+    /// <param name="records"></param>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    private static NodeRecord Find(List<NodeRecord> records, Node node)
+    {
+        foreach (NodeRecord nodeRecord in records)
+        {
+            if (nodeRecord.node == node)
+            {
+                return nodeRecord;
+            }
+        }
+
+        // there was no node within the records...
+        return null;
+    }
+
+    /// <summary>
+    /// The contains(node) method returns true only if the list contains a 
+    /// NodeRecord structure whose node member is equal to the given parameter.
+    /// </summary>
+    /// <param name="records"></param>
+    /// <param name="node"></param>
+    /// <returns></returns>
+    private static bool Contains(List<NodeRecord> records, Node node)
+    {
+        // if the given node is within the open list return true
+        foreach (NodeRecord nodeRecord in records)
+        {
+            if (nodeRecord.node == node)
+            {
+                return true;
+            }
+        }
+
+        // else the node wasnt in the list, return false
+        return false;
+    }
+
+    /// <summary>
+    /// return the NodeRecord with the smallest estimated-total-cost value, not the smallest cost-so-far
+    /// </summary>
+    /// <param name="records"></param>
+    /// <returns></returns>
+    private static NodeRecord SmallestElement(List<NodeRecord> records)
+    {
+        float lowestCost = records[0].costSoFar;
+        NodeRecord lowestNodeRecord = records[0];
+
+        foreach (NodeRecord nodeRecord in records)
+        {
+            if (nodeRecord.costSoFar < lowestCost)
+            {
+                lowestCost = nodeRecord.costSoFar;
+                lowestNodeRecord = nodeRecord;
+            }
+            // code block to be executed
+        }
+
+        // look through the open list and return the NodeRecord with the lowest cost
+        return lowestNodeRecord;
     }
 
     public delegate float Heuristic(GameObject start, GameObject tile, GameObject goal);
 
     public static float Uniform (GameObject start, GameObject tile, GameObject goal)
     {
+        /*
+        Heuristic uniform = null;
+        uniform.Invoke(start, tile, goal);
+        */
+
         return 0f;
     }
 
---
+++ HW1/Assets/Scripts/NodeRecord.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// A class for recording search statistics.
+/// </summary>
+public class NodeRecord
+{
+    // The tile game object.
+    public GameObject Tile { get; set; } = null;
+    // Set the other class properties here.
+    public Node node { get; set; } = null;
+    public NodeRecord fromNode { get; set; } = null;
+    public float costSoFar { get; set; } = 0;
+    public float cost { get; set; } = 1;
+    private Dictionary<Direction, NodeRecord> connections = new Dictionary<Direction, NodeRecord>();
+
+    public Dictionary<Direction, GameObject> getConnections()
+    {
+        return node.Connections;
+    }
+
+    // Sets the tile's color.
+    public void ColorTile(Color newColor)
+    {
+        SpriteRenderer renderer = Tile.GetComponentInChildren<SpriteRenderer>();
+        renderer.material.color = newColor;
+    }
+
+    // Displays a string on the tile.
+    public void Display(float value)
+    {
+        TextMesh text = Tile.GetComponent<TextMesh>();
+        text.text = value.ToString();
+    }
+}
